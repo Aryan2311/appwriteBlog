33f2f1ad2ddf45039758f678305799c117e2ec6c
dhatrishdixit <dhatrish29@gmail.com>
2023-12-16 12:29:10 +0530

    new commit

---
+++ src/components/header/LogoutBtn.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { useDispatch } from 'react-redux'
+import { logOut } from '../../features/authentication/authSlice'
+import authService from '../../appwrite/authentication'
+
+function LogoutBtn() {
+  const dispatch = useDispatch();
+  const logoutHandler = () => {
+        authService.logOut()
+        .then(
+            () => { dispatch(logOut()) })
+        .catch((err)=>console.log(err))
+        .finally(()=> {
+            let date = new Date();
+            console.log(`logged out at : ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()} on : ${date.getDate()}/${date.getMonth()+1}/${date.getFullYear()}`) })
+
+  }
+  return (
+    <button onClick={logoutHandler} className='inline-block px-6 py-2 duration-200 rounded-full hover:bg-blue-100'>Logout</button>
+  )
+}
+
+export default LogoutBtn
