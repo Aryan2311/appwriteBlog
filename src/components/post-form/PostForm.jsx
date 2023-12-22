cbfcd8f2a4b46b5edcb3121f0b5692a40fcb3121
dhatrishdixit <dhatrish29@gmail.com>
2023-12-22 22:47:33 +0530

    change in postForm

--- src/components/post-form/PostForm.jsx
+++ src/components/post-form/PostForm.jsx
@@ -1,6 +1,68 @@
-import React from 'react'
+import React,{useCallback,useEffect} from 'react' ;
+import {useNavigate} from 'react-router-dom' ;
+import {useSelector} from 'react-redux' ;
+import {Input,RTE,Select,Button} from '../index';
+import {useForm} from 'react-hook-form';
+import appwriteServices from '../../appwrite/config';
 
-function PostForm() {
+function PostForm({post}) {
+  const navigate = useNavigate();
+  const {register,handleSubmit,watch,control,setValue} = useForm({
+    defaultValues:{
+         title:post?.title || "",
+         slug:post?.slug || "",
+         content:post?.content || "",
+         status : post?.status || "",
+    }
+  });
+  const userData = useSelector(state=>state.auth.userData);
+  const submitHandler = async (data) => {
+    if(post){
+        const file = data.image[0]? await appwriteServices.uploadFile(data.image[0]) :null;
+        if(file){
+            await appwriteServices.deleteFile(post.FeaturedImg);
+            const updatedPost = await appwriteServices.updatePost(post.$id,{
+                ...data,
+                featuredImg : file ? file.$id : undefined
+            })
+            if(updatedPost){
+                navigate(`/post/${updatedPost.$id}`)
+            }
+        }
+       
+    }
+    else{
+        const file = data.image[0] ? await appwriteServices.uploadFile(data.image[0]) : null ;
+        if(file)
+        {
+            const newPost = await appwriteServices.createPost({
+                ...data,
+                userId:userData.$id,
+                featuredImg : file ? file.$id : undefined
+            })
+            
+        if(newPost){
+            navigate(`/post/${newPost.$id}`)
+        }
+        }
+    }
+        
+  }
+  const slugTransform = useCallback((value) => {
+       if(value && typeof value === 'string'){
+        return value
+        .trim()
+        .toLowerCase
+        .replace(/[^a-zA-Z\d\s]+/g, '-')
+        .replace(/\s/g, "-")
+       }
+       return "";
+  },[])
+  useEffect(()=>{
+
+  },[watch,slugTransform,setValue])
+
+
   return (
     <div>PostForm</div>
   )
